8446cd63eefff6f45d3d88a21ccfd756c79c30e1
MoonOnYou <onyou0031@naver.com>
2018-03-09 21:21:44 +0900

    안드로이드를 하고 싶으면 그냥냅다 외우라는 깊은 뜻이 있었다

--- Challenge01_Mission01 (234p)/src/main/java/com/example/moononyou/challenge01_mission01/MainActivity.java
+++ Challenge01_Mission01 (234p)/src/main/java/com/example/moononyou/challenge01_mission01/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.moononyou.challenge01_mission01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button=(Button)findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Toast toast= Toast.makeText(MainActivity.this,"안녕하시렵니까",Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
     }
 }
