3181cc69c433165978309492fade89616e85a7f0
MoonOnYou <onyou0031@naver.com>
2018-03-18 19:31:45 +0900

    안드로이드..내가 잘해낼 수 있을까

---
+++ Challenge01_Mission02 (236p)/src/main/java/com/example/moononyou/challenge01_mission02/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.moononyou.challenge01_mission02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
